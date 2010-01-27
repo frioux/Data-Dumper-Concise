2570b7befc519e6f0f591d26e75d2b2767e557d8
Matt S Trout <matthewt@hercule.scsys.co.uk>
2010-01-27 02:51:10 +0000

    make sure Devel::Dwarn::Dwarn(S) work

--- lib/Devel/Dwarn.pm
+++ lib/Devel/Dwarn.pm
@@ -1,6 +1,6 @@
 package Devel::Dwarn;
 
-use Data::Dumper::Concise::Sugar ();
+use Data::Dumper::Concise::Sugar;
 
 sub import {
   Data::Dumper::Concise::Sugar->export_to_level(1, @_);
--- t/dwarn.t
+++ t/dwarn.t
@@ -6,3 +6,5 @@ use Devel::Dwarn;
 use Test::More qw(no_plan);
 
 can_ok __PACKAGE__, qw{Dwarn DwarnS};
+
+can_ok Devel::Dwarn, qw{Dwarn DwarnS};
